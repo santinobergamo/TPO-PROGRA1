d797d4f2d0f4e3d419d42d36f13a5600d06015cb
Santino Bergamo <santinobergamo@gmail.com>
2023-11-15 21:12:58 -0300

    version final

---
+++ Estructura de Datos/.py
@@ -0,0 +1,4 @@
+tupla_1 = int(input("Ingrese dos valores"))
+tupla_2 = int(input("Ingrese dos valores"))
+def dominio ():
+    
---
+++ TDA/Ejercicio 2.py
@@ -0,0 +1,40 @@
+def inicializarPila():
+    pila= []
+    return pila
+
+def apilar(pila, dato):
+    pila.append(dato)
+    
+def desapilar(pila):
+    pila.pop()
+
+def pilaVacia(pila):
+    return len(pila)== 0
+
+def tope(pila):
+    return pila[-1]
+
+def invertirOrden(pila):
+    return pila[::-1]
+
+
+
+
+
+
+
+
+pila= inicializarPila()
+apilar(pila, 1)
+apilar(pila, 2)
+apilar(pila, 3)
+
+
+print("Pila: ",  pila)
+print("Pila invertida: ", invertirOrden(pila))
+
+
+
+
+
+
